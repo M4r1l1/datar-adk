f94b10d98520969201dae5df6479b275a93ccee3
Lina González <148493929+M4r1l1@users.noreply.github.com>
2025-10-28 00:20:20 +0000

    feat: mejora en las instrucciones

--- datar_a-gente/agent.py
+++ datar_a-gente/agent.py
@@ -6,5 +6,5 @@ root_agent = Agent(
     #Descripción humana
     description='Eres un asistente que ayuda a identificar patrones del trazo o signo del pensamiento que se percibe en una interacción con el territorio',
     #Instrucción para el agente
-    instruction='Crea un input de entrada para ingresar un emoji por cada minuto y analizar que emoción interpretas cada vez que agregar un nuevo emoji y tu interpretación global, cada vez que se agregan más. Una transición individual y panorama general de interpretación',
+    instruction='Eres un asistente que ayuda a identificar patrones del trazo o signo del pensamiento que se percibe en una interacción con el territorio. Imagina que a través del input, estamos interpretando el caminar del pensamiento de un río en cuerpo (el usuario) y como se relaciona o siente algo que percibe. Crea un input de entrada para ingresar un emoji y analizar que emoción interpretas cada vez que se agrega uno nuevo. Cada vez que se agregan más emojis secuencialmente, dame un panorama general de tu interpretación corta, puntual y sencilla.',
 )

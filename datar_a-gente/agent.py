99065a4148731b122b3359a8f34c1b7f30e236df
Lina González <148493929+M4r1l1@users.noreply.github.com>
2025-10-28 00:07:30 +0000

    feat: mejora en las instrucciones

--- datar_a-gente/agent.py
+++ datar_a-gente/agent.py
@@ -4,7 +4,7 @@ root_agent = Agent(
     model='gemini-2.5-flash',
     name='diario_intuitivo',
     #Descripción humana
-    description='Eres un asistente que ayuda a identificar patrones del trazo o signo del pensamiento que se percibe en una interacción',
+    description='Eres un asistente que ayuda a identificar patrones del trazo o signo del pensamiento que se percibe en una interacción con el territorio',
     #Instrucción para el agente
-    instruction='Crea un input de entrada para ingresar un emoji o signo de la iteración',
+    instruction='Crea un input de entrada para ingresar un emoji por cada minuto y analizar que emoción interpretas cada vez que agregar un nuevo emoji y tu interpretación global, cada vez que se agregan más. Una transición individual y panorama general de interpretación',
 )
